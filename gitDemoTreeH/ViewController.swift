bb734ae729839cc85fad37d486d64d2ac00b5f10
Eskills <esebetseba@gmail.com>
2016-05-01 08:19:36 +0200

    Initial Commit

---
+++ gitDemoTreeH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoTreeH
+//
+//  Created by Edmond Sebetseba on 2016/05/01.
+//  Copyright © 2016 Edmond Sebetseba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ gitDemoTreeHTests/gitDemoTreeHTests.swift
@@ -0,0 +1,36 @@
+//
+//  gitDemoTreeHTests.swift
+//  gitDemoTreeHTests
+//
+//  Created by Edmond Sebetseba on 2016/05/01.
+//  Copyright © 2016 Edmond Sebetseba. All rights reserved.
+//
+
+import XCTest
+@testable import gitDemoTreeH
+
+class gitDemoTreeHTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
